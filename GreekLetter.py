ade14198c1b05327cdc3924043259041bca5ff28
Robert Moss <mossr@wit.edu>
2014-10-16 19:17:48 -0400

    Initial commit

---
+++ GreekLetter.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sublime, sublime_plugin
+
+class GreekLetterExpandCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		greekmappings = {
+		'Alpha': 'Α',
+		'alpha': 'α',
+		'Beta': 'Β',
+		'beta': 'β',
+		'Gamma': 'Γ',
+		'gamma': 'γ',
+		'Delta': 'Δ',
+		'delta': 'δ',
+		'Epsilon': 'Ε',
+		'epsilon': 'ε',
+		'Zeta': 'Ζ',
+		'zeta': 'ζ',
+		'Eta': 'Η',
+		'eta': 'η',
+		'Theta': 'Θ',
+		'theta': 'θ',
+		'Iota': 'Ι',
+		'iota': 'ι',
+		'Kappa': 'Κ',
+		'kappa': 'κ',
+		'Lambda': 'Λ',
+		'lambda': 'λ',
+		'Mu': 'Μ',
+		'mu': 'μ',
+		'Nu': 'Ν',
+		'nu': 'ν',
+		'Xi': 'Ξ',
+		'xi': 'ξ',
+		'Omicron': 'Ο',
+		'omicron': 'ο',
+		'Pi': 'Π',
+		'pi': 'π',
+		'Rho': 'Ρ',
+		'rho': 'ρ',
+		'Sigma': 'Σ',
+		'sigma': 'σ',
+		'Tau': 'Τ',
+		'tau': 'τ',
+		'Upsilon': 'Υ',
+		'upsilon': 'υ',
+		'Phi': 'Φ',
+		'phi': 'φ',
+		'Chi': 'Χ',
+		'chi': 'χ',
+		'Psi': 'Ψ',
+		'psi': 'ψ',
+		'Omega': 'Ω',
+		'omega': 'ω'
+		}
+		self.view.run_command("expand_selection",{"to": "word"})
+		sels = self.view.sel()
+		for sel in sels:
+			key = self.view.substr(sel)
+			replacement = greekmappings[key]
+			print replacement
+			self.view.replace(edit,sel,replacement.decode('utf-8'))
+		end = self.view.sel()[0].b
+		pt = sublime.Region(end, end)
+		self.view.sel().clear()
+		self.view.sel().add(pt)
+
---
+++ README.md
@@ -0,0 +1,59 @@
+GreekLetters
+===========
+
+Converts Greek alphabet to utf-8 Greek letter in Sublime Text 2.
+
+Hitting `shift`-`tab` will replace the previous word with a utf-8 Greek letter, according to the following dictionary:
+
+    {
+	'Alpha': 'Α',
+	'alpha': 'α',
+	'Beta': 'Β',
+	'beta': 'β',
+	'Gamma': 'Γ',
+	'gamma': 'γ',
+	'Delta': 'Δ',
+	'delta': 'δ',
+	'Epsilon': 'Ε',
+	'epsilon': 'ε',
+	'Zeta': 'Ζ',
+	'zeta': 'ζ',
+	'Eta': 'Η',
+	'eta': 'η',
+	'Theta': 'Θ',
+	'theta': 'θ',
+	'Iota': 'Ι',
+	'iota': 'ι',
+	'Kappa': 'Κ',
+	'kappa': 'κ',
+	'Lambda': 'Λ',
+	'lambda': 'λ',
+	'Mu': 'Μ',
+	'mu': 'μ',
+	'Nu': 'Ν',
+	'nu': 'ν',
+	'Xi': 'Ξ',
+	'xi': 'ξ',
+	'Omicron': 'Ο',
+	'omicron': 'ο',
+	'Pi': 'Π',
+	'pi': 'π',
+	'Rho': 'Ρ',
+	'rho': 'ρ',
+	'Sigma': 'Σ',
+	'sigma': 'σ',
+	'Tau': 'Τ',
+	'tau': 'τ',
+	'Upsilon': 'Υ',
+	'upsilon': 'υ',
+	'Phi': 'Φ',
+	'phi': 'φ',
+	'Chi': 'Χ',
+	'chi': 'χ',
+	'Psi': 'Ψ',
+	'psi': 'ψ',
+	'Omega': 'Ω',
+	'omega': 'ω'
+	}
+	  
+Inspired by the GreekTex package written by Neil Anderson: [https://github.com/neilanderson/GreekTeX](https://github.com/neilanderson/GreekTeX)
